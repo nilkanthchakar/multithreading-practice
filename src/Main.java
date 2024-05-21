787bcefa17449545020232788f5984aa71d91934
nilkanth <chakarnilkanth@gmail.com>
2024-05-22 00:47:11 +0530

    test

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ class Mythread extends Thread{
 
         for(int i = 0; i < 11; i++){
 
-            System.out.println(i);
+            System.out.println("Thread one i is : " + i);
 
             try{
                 sleep(1000);
@@ -19,10 +19,38 @@ class Mythread extends Thread{
 
 }
 
+class thread2 implements Runnable{
+
+
+    public void run(){
+        for(int i = 10; i > 0; i--){
+            System.out.println("thread 2 i is " + i);
+            try{
+                Thread.sleep(1000);
+            }catch(Exception e){
+
+            }
+
+        }
+    }
+
+
+}
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Mythread mythread = new Mythread();
+
+
+        thread2 thread2 = new thread2();
+        Thread t1 = new Thread(thread2);
+        mythread.start();
+        t1.start();
+
+
+
+
     }
 
 
